bb91a30388d952200ffa64b428c55c704ed6822f
Travis Quigg <62739843+tquigg96@users.noreply.github.com>
2021-11-10 22:23:32 -0800

    Delete commands directory

--- commands/image.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'image',
-	description: "Import an image",
-	execute(message, args){
-		message.channel.send('ImageRequest');
-	}
-}
--- commands/mint.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'mint',
-	description: "this is a nft minter request",
-	execute(message, args){
-		message.channel.send('Minty');
-	}
-}
